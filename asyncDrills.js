ef4e565054f4909b154e5fba4312edc7f17ad3b2
heathertill <heather.till@charter.net>
2018-11-05 23:22:28 -0600

    finished the lab

--- asyncDrills.js
+++ asyncDrills.js
@@ -5,13 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
     function myFunc() {
         console.log('Hello World!!!');
         funcValue = Math.floor(Math.random() * (100 - 0)) + 0;
-        // funcValue = 10;
         return funcValue;
     }
     console.log(myFunc());
 
     function anotherFunc() {
-    
         let anotherFuncValue = funcValue * 4;
         console.log(anotherFuncValue)
         return anotherFuncValue;
@@ -35,14 +33,9 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 3000);
     }
     
-    
-    
+
     getWords(['Pizza', 'Hamburger', 'Hotdog', 'Ice Cream'])
     
-
-
-
-
     function countdown(num, callback) {
         let x = num;
         let count = setInterval(function() {
@@ -53,30 +46,78 @@ document.addEventListener('DOMContentLoaded', function() {
                 callback()
             }
         }, 1000) 
-      
     }
-    
     
     function done() {
         console.log('The function is done!');
     }
     
-    
     countdown(5, done);
 
+let thisNum = trueFalse();
+
+
+function trueFalse() {
+    let randValue = Math.floor(Math.random() * 10);
+    return (randValue % 2 == 0) ? true : false;
+}
+
+
+
+let orderChickenSandwich = new Promise(function (resolve, reject) {
+    if (thisNum === true) {
+        let order = {
+            sandwich: 'chicken',
+            veggie: 'lettuce'
+        };
+        resolve(order);
+    } else {
+        let reason = new Error('Something went wrong with your order');
+        reject(reason);
+    }
+    resolve(message)
+});
+
+
+let orderFood = function() {
+  orderChickenSandwich
+  .then(function(a) {
+      console.log(`You ordered a ${a.sandwich} sandwich with ${a.veggie}. Would you like a drink with that?`)
+  })
+  .catch(function(b) {
+     console.log(b.message)
+  })
+}
+
+
+orderFood()
 
 
 
 
+ var chain = new Promise(
+    function (resolve, reject) {
+        setTimeout(() => {
+            resolve(1);
+          }, 2000);
+    });
+    
+    chain
+    .then((val) => {
+        console.log(val)
+        return val * 2
+    })
+    .then((val) => {
+        console.log(val);
+        return val * 4
+    })
+    .then((val) => {
+        console.log(val);
+        return val * 6
+    })
+    .then((val) => {
+        console.log(val)
+    })
 
 
-
-
-// 
-// 
-// 
-// 
-// 
-// 
-// 
 });
